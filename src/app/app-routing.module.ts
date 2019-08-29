e39fcb1251d60d4d4d7f939b56103af786ef86c9
i-bilobran <ihor.bilobran@sombrainc.com>
2019-08-29 18:41:15 +0300

    animations init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { CardsPhysicsComponent } from './cards-physics/cards-physics.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'cards-physics', pathMatch: 'full' },
+  { path: 'cards-physics', component: CardsPhysicsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/cards-physics/cards-physics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-physics',
+  templateUrl: './cards-physics.component.html',
+  styleUrls: ['./cards-physics.component.scss']
+})
+export class CardsPhysicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
